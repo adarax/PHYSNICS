174a6be77ccf95788e4e1f89d95e45e5a8034c60
adarax <adam88183@gmail.com>
2023-02-24 10:32:06 -0500

    Add initialize() method without body

--- app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
+++ app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
@@ -1,9 +1,15 @@
 package edu.vanier.physnics.mainmenu;
 
+import edu.vanier.physnics.App;
+import javafx.fxml.FXML;
+
 /**
  *
  * @author adarax
  */
-public class MainMenuController {
-    
+public class MainMenuController extends App {
+    @FXML
+    public void initialize() {
+        // TODO
+    }
 }
