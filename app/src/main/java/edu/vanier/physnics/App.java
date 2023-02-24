d0d8d5731a10121dd707d4db5be69126c4224bd7
adarax <adam88183@gmail.com>
2023-02-24 10:31:22 -0500

    Make initial start() method to open mainmenu

--- app/src/main/java/edu/vanier/physnics/App.java
+++ app/src/main/java/edu/vanier/physnics/App.java
@@ -1,14 +1,33 @@
 package edu.vanier.physnics;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import edu.vanier.physnics.mainmenu.MainMenuController;
 
 public class App extends Application {
     public Stage mainMenu;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        mainMenu = stage;
         
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainmenu.fxml"));
+
+        MainMenuController mwc = new MainMenuController();
+        loader.setController(mwc);
+        
+        Parent root = loader.load();               
+        Scene scene = new Scene(root, 600, 400);
+        
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.sizeToScene();
+        stage.getIcons().add(new Image("/images/app_icon.png"));
+        stage.show();
     }
     
     public static void main(String[] args) {
