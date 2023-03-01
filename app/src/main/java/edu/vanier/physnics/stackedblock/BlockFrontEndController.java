62bbe51aec48f3ff53764e1772b38933312a7f9c
adarax <adam88183@gmail.com>
2023-03-01 13:42:02 -0500

    Add empty initialize method

--- app/src/main/java/edu/vanier/physnics/stackedblock/BlockFrontEndController.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/BlockFrontEndController.java
@@ -1,9 +1,16 @@
 package edu.vanier.physnics.stackedblock;
 
+import javafx.fxml.FXML;
+
 /**
  *
  * @author adarax
  */
 public class BlockFrontEndController {
     
+    
+    @FXML
+    public void initialize() {
+        
+    }
 }
