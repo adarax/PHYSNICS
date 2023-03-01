f485baff4ef51102e86fb57c9c7acc858f66d6f5
Adam Purcell <adam88183@gmail.com>
2023-02-28 21:40:19 -0500

    Setup icons for the various simulations

--- app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
+++ app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
@@ -8,8 +8,25 @@ import javafx.fxml.FXML;
  * @author adarax
  */
 public class MainMenuController extends App {
+    
+    public void conservationPressed() {
+        
+    }
+    
+    public void stackedBlockPressed() {
+        
+    }
+    
+    public void projectilePressed() {
+        
+    }
+    
+    public void centripetalPressed() {
+        
+    }
+    
     @FXML
     public void initialize() {
-        // TODO
+        
     }
 }
