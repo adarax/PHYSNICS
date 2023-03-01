eea8c13173926982d040acf4899545c7dd332a79
Adam Purcell <adam88183@gmail.com>
2023-02-28 22:26:15 -0500

    Add methods to handle button press of main menu UI buttons

--- app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
+++ app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
@@ -2,6 +2,8 @@ package edu.vanier.physnics.mainmenu;
 
 import edu.vanier.physnics.App;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -9,24 +11,57 @@ import javafx.fxml.FXML;
  */
 public class MainMenuController extends App {
     
-    public void conservationPressed() {
-        
+    @FXML
+    private ImageView buttonHelp;
+    
+    @FXML
+    private ImageView buttonConservation;
+    
+    @FXML
+    private ImageView buttonStackedBlock;
+    
+    @FXML
+    private ImageView buttonProjectile;
+    
+    @FXML
+    private ImageView buttonCentripetal;
+
+    
+    public void handleConservationPressed(MouseEvent e) {
+        System.out.println("Conservation of energy pressed");
+
     }
     
-    public void stackedBlockPressed() {
-        
+    public void handleStackedBlockPressed(MouseEvent e) {
+        System.out.println("Stacked block pressed");
+
     }
     
-    public void projectilePressed() {
-        
+    public void handleProjectilePressed(MouseEvent e) {
+        System.out.println("Projectile motion pressed");
+
     }
     
-    public void centripetalPressed() {
-        
+    public void handleCentripetalPressed(MouseEvent e) {
+        System.out.println("Centripetal pressed");
+
+    }
+    
+    public void handleHelpPressed(MouseEvent e) {
+        System.out.println("Help was requested");
     }
     
     @FXML
     public void initialize() {
+        /**
+         * setOnMouseClicked() is used, not setOnAction(), since the buttons are
+         * actually images.
+         */
         
+        buttonConservation.setOnMouseClicked(e -> handleConservationPressed(e));
+        buttonStackedBlock.setOnMouseClicked(e -> handleStackedBlockPressed(e));
+        buttonProjectile.setOnMouseClicked(e -> handleProjectilePressed(e));
+        buttonCentripetal.setOnMouseClicked(e -> handleCentripetalPressed(e));
+        buttonHelp.setOnMouseClicked(e -> handleHelpPressed(e));
     }
 }
