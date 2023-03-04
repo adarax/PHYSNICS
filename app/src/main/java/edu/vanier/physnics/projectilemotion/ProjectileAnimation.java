7b55e8db9daddfe7498c200710d4cceaf8982217
vireshpatel43 <viresh.p2003@gmail.com>
2023-03-04 13:34:32 -0500

    Preliminary animation of ball

--- app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileAnimation.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileAnimation.java
@@ -4,10 +4,32 @@
  */
 package edu.vanier.physnics.projectilemotion;
 
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
+
 /**
  *
  * @author vires
  */
 public class ProjectileAnimation {
     
+    public static void ballAnimation(Circle ball) {
+        Path path = new Path();
+        path.getElements().add(new MoveTo(20, 20));
+        path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
+        path.getElements().add(new CubicCurveTo(0, 120, 0, 240, 380, 240));
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(4000));
+        pathTransition.setPath(path);
+        pathTransition.setNode(ball);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setAutoReverse(true);
+        pathTransition.play();
+    }
 }
