09a9a810e22fa3a0c899b52f27f28776be38532d
vireshpatel43 <viresh.p2003@gmail.com>
2023-03-04 13:35:39 -0500

    Add empty methods for UI controls and event handlers

--- app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileController.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileController.java
@@ -4,10 +4,146 @@
  */
 package edu.vanier.physnics.projectilemotion;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXSlider;
+import io.github.palexdev.materialfx.controls.MFXToggleButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+
 /**
- *
+ * Instantiates the FXML UI controls and shapes to be used in the animation.
+ * Handles action events.
  * @author vires
  */
 public class ProjectileController {
-    
+    @FXML
+    private MenuItem menubuttonExit;
+
+    @FXML
+    private MenuItem menubuttonMainMenu;
+
+    @FXML
+    private MenuItem menubuttonConservation;
+
+    @FXML
+    private MenuItem menubuttonCentripetal;
+
+    @FXML
+    private MenuItem menubuttonProjectile;
+
+    @FXML
+    private ImageView buttonDarkMode;
+
+    @FXML
+    private MFXSlider sliderInitialVelocity;
+
+    @FXML
+    private MFXSlider sliderLaunchAngle;
+
+    @FXML
+    private MFXSlider sliderGravity;
+
+    @FXML
+    private MFXButton buttonClear;
+
+    @FXML
+    private ImageView buttonPlay;
+
+    @FXML
+    private ImageView buttonPause;
+
+    @FXML
+    private ImageView buttonReset;
+
+    @FXML
+    private ImageView buttonHelp;
+
+    @FXML
+    private ImageView buttonGraph;
+
+    @FXML
+    private ImageView buttonLogo;
+
+    @FXML
+    private Circle projectileBall;
+
+
+
+    // Event handlers for the action events.
+    public void handlePlay(MouseEvent e) {
+        // Play the simulation
+        System.out.println("Play");
+        ProjectileAnimation.ballAnimation(projectileBall);
+    }
+
+    public void handlePause(MouseEvent e) {
+        // Pause the simulation
+    }
+
+    public void handleReset(MouseEvent e) {
+        // Reset the simulation
+    }
+
+    public void handleHelp(MouseEvent e) {
+        // Open the help screen
+    }
+
+    public void handleClear(MouseEvent e) {
+        // Reset the animation and bring the sliders to default values
+    }
+
+
+    @FXML
+    public void initialize() {
+
+        buttonClear.setOnAction(e -> {
+            // Resets the animation and brings the sliders to default values
+        });
+
+        menubuttonCentripetal.setOnAction(e -> {
+            // Go to centripetal force screen
+        });
+
+        menubuttonConservation.setOnAction(e -> {
+            // Go to conservation of energy screen
+        });
+
+        menubuttonExit.setOnAction(e -> {
+            // Exit the app
+        });
+
+        menubuttonProjectile.setOnAction(e -> {
+            // Go to projectile motion screen
+        });
+
+        sliderInitialVelocity.setOnMouseDragged(e -> {
+            // Change the initial velocity
+        });
+
+        sliderLaunchAngle.setOnMouseDragged(e -> {
+            // Change the launch angle
+        });
+
+        sliderGravity.setOnMouseDragged(e -> {
+            // Change the force of gravity
+        });
+
+        buttonPlay.setOnMouseClicked(e -> {
+            handlePlay(e);
+        });
+
+        buttonReset.setOnMouseClicked(e -> {
+            // Calls handleReset
+        });
+
+        buttonPause.setOnMouseClicked(e -> {
+            // Calls handlePause
+        });
+
+    }
+
 }
