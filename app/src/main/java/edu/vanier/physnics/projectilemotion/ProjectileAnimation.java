266233f5f6bca408f8e570764a9a13d4c41f904a
vireshpatel43 <viresh.p2003@gmail.com>
2023-03-04 19:41:00 -0500

    Animate based on a quadratic curve

--- app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileAnimation.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileAnimation.java
@@ -6,10 +6,11 @@ package edu.vanier.physnics.projectilemotion;
 
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
+import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.CubicCurveTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.QuadCurveTo;
 import javafx.util.Duration;
 
 /**
@@ -19,15 +20,26 @@ import javafx.util.Duration;
 public class ProjectileAnimation {
     
     public static void ballAnimation(Circle ball) {
+        
         Path path = new Path();
-        path.getElements().add(new MoveTo(20, 20));
-        path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
-        path.getElements().add(new CubicCurveTo(0, 120, 0, 240, 380, 240));
+
+        MoveTo moveTo = new MoveTo();
+        moveTo.setX(50);
+        moveTo.setY(800);
+
+        QuadCurveTo quadTo = new QuadCurveTo();
+        quadTo.setControlX(725);
+        quadTo.setControlY(300);
+        quadTo.setX(1500);
+        quadTo.setY(800);
+        path.getElements().add(moveTo);
+        path.getElements().add(quadTo);
+        
         PathTransition pathTransition = new PathTransition();
+        
         pathTransition.setDuration(Duration.millis(4000));
         pathTransition.setPath(path);
         pathTransition.setNode(ball);
-        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
         pathTransition.setCycleCount(Timeline.INDEFINITE);
         pathTransition.setAutoReverse(true);
         pathTransition.play();
