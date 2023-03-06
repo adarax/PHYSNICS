f9d11846c3cf6fb478404a3b24e9548f99e0dd83
Viresh Patel <2128027@edu.vaniercollege.qc.ca>
2023-03-06 09:30:13 -0500

    Start implmenting equation for projectile motion

--- app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileEquations.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/ProjectileEquations.java
@@ -9,5 +9,27 @@ package edu.vanier.physnics.projectilemotion;
  * @author vires
  */
 public class ProjectileEquations {
+    double initialVelocity;
+    double launchAngle;
+    double gravityAccel;
+    double sinAngle = Math.toDegrees(Math.sin(launchAngle));
+    
+    public double getMaxHeight() {
+        
+       double maxHeight = Math.pow(initialVelocity, 2);
+        
+       return 0.0;
+    }
+    
+    public static double getXdisplacement() {
+        return 0.0;
+    }
+    
+    public double getFlightTime() {
+        
+        double flightTime = (2.0 * initialVelocity * sinAngle) / gravityAccel;
+        return flightTime;
+    }
+    
     
 }
