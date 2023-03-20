f83c0378631d8bc485a4445903b4ce62ff1c5b0d
adarax <adam88183@gmail.com>
2023-03-20 12:42:57 -0400

    Add all necessary physics formulas for the simulation

--- app/src/main/java/edu/vanier/physnics/stackedblock/BlockFormulas.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/BlockFormulas.java
@@ -6,4 +6,34 @@ package edu.vanier.physnics.stackedblock;
  */
 public class BlockFormulas {
     
+    // Value in m/s/s
+    private final double GRAVITATIONAL_ACCELERATION = 9.81;
+    
+    // Var-args treats the inputs as an array
+    // @return Value in Newtons
+    public Vector calculateNormalForceVector(double... contributingMassInKg)
+    {
+        double sumOfMasses = 0;
+        
+        for (double mass : contributingMassInKg)
+        {
+            sumOfMasses += mass;
+        }
+        
+        double magnitudeOfForce = sumOfMasses * this.GRAVITATIONAL_ACCELERATION;
+        
+        // Normal force always points opposite to direction of gravity.
+        // In the case of this simulation, gravity always points 270
+        // degrees from the +x-axis since the floor is flat
+        double directionOfForce = 90;
+        
+        return new Vector(magnitudeOfForce, directionOfForce);
+    }
+    
+    
+    public double calculateForceOfFriction(double coefficientOfFriction, double normalForce)
+    {
+        return coefficientOfFriction * normalForce;
+    }
+    
 }
