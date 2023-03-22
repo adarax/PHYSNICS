da378779b6e7eb57cb54c3e9e9a36507735a16bc
adarax <adam88183@gmail.com>
2023-03-22 15:01:10 -0400

    Format code (curly braces on their own line)

--- app/src/main/java/edu/vanier/physnics/stackedblock/Block.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/Block.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
  * @author adarax
  */
 public class Block {
-    
+
     private double mass;
-    
+
     // Either 0, which is the bottom block
     // or 1, which is the top block
     private int blockNumber;
@@ -22,41 +22,47 @@ public class Block {
         this.blockNumber = blockNumber;
         this.forcesExperienced = forcesExperienced;
     }
-    
+
     public Vector calculateNetForceAndDirection(ArrayList<Vector> forcesExperienced)
     {
         // top block treated differently from bottom (friction mainly)
         // sum up x and y components, figure out magnitude and direction
-        
+
         return null;
     }
-    
+
     public void drawFreeBodyDiagram(ArrayList<Vector> forcesExperienced)
     {
         // Animate vectors (arrows) onto block
     }
 
-    public double getMass() {
+    public double getMass()
+    {
         return mass;
     }
 
-    public void setMass(double mass) {
+    public void setMass(double mass)
+    {
         this.mass = mass;
     }
 
-    public int getBlockNumber() {
+    public int getBlockNumber()
+    {
         return blockNumber;
     }
 
-    public void setBlockNumber(int blockNumber) {
+    public void setBlockNumber(int blockNumber)
+    {
         this.blockNumber = blockNumber;
     }
 
-    public ArrayList<Vector> getForcesExperienced() {
+    public ArrayList<Vector> getForcesExperienced()
+    {
         return forcesExperienced;
     }
 
-    public void setForcesExperienced(ArrayList<Vector> forcesExperienced) {
+    public void setForcesExperienced(ArrayList<Vector> forcesExperienced)
+    {
         this.forcesExperienced = forcesExperienced;
     }
 }
