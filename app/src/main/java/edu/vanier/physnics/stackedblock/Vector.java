29e0f3a3307c16216b53b47cf43412d53eb36f52
Adam Purcell <adam88183@gmail.com>
2023-03-26 01:16:16 -0400

    Remove unnecessary setters, fix comments

--- app/src/main/java/edu/vanier/physnics/stackedblock/Vector.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/Vector.java
@@ -7,19 +7,19 @@ import java.util.ArrayList;
  * @author adarax
  */
 public class Vector {
-    
-    private double magnitudeInNewtons;
-    
+
+    private final double magnitudeInNewtons;
+
     // Values range from 0 to 360 degrees
-    private double directionInDegrees;
-    
-    
+    private final double directionInDegrees;
+
     public Vector(double magnitudeInNewtons, double directionInDegrees)
     {
         this.magnitudeInNewtons = magnitudeInNewtons;
         this.directionInDegrees = directionInDegrees;
     }
     
+    // This method may be useful for drawing the vectors onto the screen
     public int findQuadrant(double directionInDegrees)
     {
         // Handle any illegal direction values
@@ -27,58 +27,42 @@ public class Vector {
         {
             return 0;
         }
-        
+
         if (directionInDegrees <= 90)
         {
             return 1;
-        }
-        else if (directionInDegrees <= 180)
+        } else if (directionInDegrees <= 180)
         {
             return 2;
-        }    
-        else if (directionInDegrees <= 270)
+        } else if (directionInDegrees <= 270)
         {
             return 3;
-        }
-        else
+        } else
         {
             return 4;
         }
     }
-    
+
     public ArrayList<Double> vectorToComponents(double magnitudeInNewtons, double directionInDegrees)
     {
         ArrayList<Double> asComponents = new ArrayList<>();
-        
+
         double xComponent = Math.cos(directionInDegrees) * magnitudeInNewtons;
         double yComponent = Math.sin(directionInDegrees) * magnitudeInNewtons;
-        
+
         asComponents.add(xComponent);
         asComponents.add(yComponent);
-        
+
         return asComponents;
     }
 
-    
-    // Not sure if setters are required for this class
-    
     public double getMagnitudeInNewtons()
     {
         return magnitudeInNewtons;
     }
 
-    public void setMagnitudeInNewtons(double magnitudeInNewtons)
-    {
-        this.magnitudeInNewtons = magnitudeInNewtons;
-    }
-
     public double getDirectionInDegrees()
     {
         return directionInDegrees;
-    }
-
-    public void setDirectionInDegrees(double directionInDegrees)
-    {
-        this.directionInDegrees = directionInDegrees;
     }
 }
