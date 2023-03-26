b33fdef42262188f73dbc559b62a6d58a25f3678
Adam Purcell <adam88183@gmail.com>
2023-03-26 01:11:04 -0400

    Update situateBlocks() to allow for continuous resizing of a given Block instance

--- app/src/main/java/edu/vanier/physnics/stackedblock/BlockAnimation.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/BlockAnimation.java
@@ -1,9 +1,11 @@
 package edu.vanier.physnics.stackedblock;
 
 import java.util.ArrayList;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 /**
  *
@@ -20,8 +22,14 @@ public class BlockAnimation {
      * Block parameters are used to determine the size of the blocks. A greater
      * mass of the block will make the block appear bigger.
      *
-     * animationPane parameter is the Pane in which the animation takes place in
-     * the GUI.
+     * The animationPane parameter is the Pane in which the animation takes
+     * place in the GUI.
+     *
+     * In this library, Rectangles are drawn from top to bottom, so anything the
+     * block must be above must be subtracted from the paneHeight.
+     *
+     * When calculating the Y coordinates, the -1 is to prevent overlap with the
+     * border of the Gridpane.
      *
      * @param topBlock
      * @param bottomBlock
@@ -32,9 +40,8 @@ public class BlockAnimation {
         double paneWidth = animationPane.getWidth();
         double paneHeight = animationPane.getHeight();
 
-        // Drawn from top to bottom, so anything the block must be above must
-        // be subtracted from the paneHeight
-        // The -1 is to prevent overlap with the border of the gridpane
+        drawBlocks(topBlock, bottomBlock);
+
         double bottomBlockYCoordinate = paneHeight - this.floorHeight
                 - bottomBlock.getDrawingHeight() - 1;
 
@@ -42,14 +49,33 @@ public class BlockAnimation {
                 - bottomBlock.getDrawingHeight()
                 - topBlock.getDrawingHeight() - 1;
 
-        // Position the blocks in the center
+        // Center the blocks
         bottomBlock.setLayoutX(paneWidth / 2 - bottomBlock.getDrawingWidth() / 2);
         bottomBlock.setLayoutY(bottomBlockYCoordinate);
 
         topBlock.setLayoutX(paneWidth / 2 - topBlock.getDrawingWidth() / 2);
         topBlock.setLayoutY(topBlockYCoordinate);
 
-        animationPane.getChildren().addAll(bottomBlock, topBlock);
+        animationPane.getChildren().addAll(topBlock, bottomBlock);
+    }
+
+    public void drawBlocks(Block topBlock, Block bottomBlock)
+    {
+        for (Block block : new ArrayList<Block>() {
+            {
+                add(topBlock);
+                add(bottomBlock);
+            }
+        })
+        {
+            block.determineAndSetDrawingHeight();
+            block.determineAndSetDrawingWidth();
+            block.setBlockDrawing(new Rectangle(block.getDrawingWidth(), block.getDrawingHeight()));
+            block.getBlockDrawing().setFill(block.determineColor());
+            block.setNametag(new Label(block.getName()));
+            block.getNametag().setFont(new Font("SansSerif Bold", block.determineLabelFontSize()));
+            block.drawBlock();
+        }
     }
 
     public void drawFloor(Pane animationPane)
@@ -60,7 +86,7 @@ public class BlockAnimation {
         Rectangle floorDrawing = new Rectangle(0, paneHeight - this.floorHeight - 1,
                 paneWidth - 1, this.floorHeight);
         floorDrawing.setFill(Color.web("807979"));
-        
+
         animationPane.getChildren().add(floorDrawing);
     }
 
