55e40d344823d4de83a478d9aeb17c3469d41060
Adam Purcell <adam88183@gmail.com>
2023-03-26 01:12:09 -0400

    Optimize vector and magnitude calculations for friction

--- app/src/main/java/edu/vanier/physnics/stackedblock/BlockFormulas.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/BlockFormulas.java
@@ -7,63 +7,63 @@ import java.util.ArrayList;
  * @author adarax
  */
 public class BlockFormulas {
-    
+
     // Value in m/s^2
     private final double GRAVITATIONAL_ACCELERATION = 9.81;
-    
+
     /**
-     * Calculates the total normal force on a block and returns it as a vector
-     * pointing 90 degrees.
-     * 
+     * Calculates the total normal force on a block.
+     *
      * @param contributingMassInKg
-     * @return Vector who's magnitude is the total normal force on the block.
+     * @return Magnitude of the total normal force on the block.
      */
-    public Vector calculateNormalForceVector(double... contributingMassInKg)
+    public double calculateNormalForceMagnitude(double... contributingMassInKg)
     {
         double sumOfMasses = 0;
-        
+
         for (double mass : contributingMassInKg)
         {
             sumOfMasses += mass;
         }
-        
-        double magnitudeOfForce = sumOfMasses * this.GRAVITATIONAL_ACCELERATION;
-        
-        // Normal force always points opposite to direction of gravity.
-        // In the case of this simulation, gravity always points 270
-        // degrees from the +x-axis since the floor is flat
-        double directionOfForce = 90;
-        
-        return new Vector(magnitudeOfForce, directionOfForce);
+
+        double magnitudeOfForce = sumOfMasses * GRAVITATIONAL_ACCELERATION;
+
+        return magnitudeOfForce;
     }
-    
-    
-    public double calculateForceOfFriction(double coefficientOfFriction, double normalForce)
+
+    public Vector calculateFrictionVector(double coefficientOfFriction, double normalForceVector, Vector correspondingForceVector)
     {
-        return coefficientOfFriction * normalForce;
+        double frictionVectorMagnitude = coefficientOfFriction * normalForceVector;
+        double frictionVectorDirection = correspondingForceVector.getDirectionInDegrees() + 180;
+
+        if (frictionVectorDirection >= 360)
+        {
+            frictionVectorDirection -= 360;
+        }
+
+        return new Vector(frictionVectorMagnitude, frictionVectorDirection);
     }
-    
-    
+
     /**
      * Calculates the magnitude and direction of a resultant vector by adding
      * the components of all forces experienced by the block.
-     * 
+     *
      * @param forcesExperienced
      * @return The resultant vector of all forces from forcesExperienced.
      */
     public Vector calculateNetForceAndDirection(ArrayList<Vector> forcesExperienced)
     {
         double sumXComponents = 0, sumYComponents = 0;
-        
+
         for (Vector forceVector : forcesExperienced)
         {
             sumXComponents += Math.cos(forceVector.getDirectionInDegrees()) * forceVector.getMagnitudeInNewtons();
             sumYComponents += Math.sin(forceVector.getDirectionInDegrees()) * forceVector.getMagnitudeInNewtons();
         }
-        
+
         double magnitudeOfResultant = Math.sqrt(Math.pow(sumXComponents, 2) + Math.pow(sumYComponents, 2));
         double directionOfResultant = Math.atan(sumYComponents / sumXComponents);
-        
+
         return new Vector(magnitudeOfResultant, directionOfResultant);
     }
 }
