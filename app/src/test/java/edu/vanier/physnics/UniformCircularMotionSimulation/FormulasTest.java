46be81bc18d9f5f101b8ea8c0dc8661e3bd8b75b
Victor-Pen <104034719+Victor-Pen@users.noreply.github.com>
2023-04-17 09:27:23 -0400

    added test methods

--- app/src/test/java/edu/vanier/physnics/UniformCircularMotionSimulation/FormulasTest.java
+++ app/src/test/java/edu/vanier/physnics/UniformCircularMotionSimulation/FormulasTest.java
@@ -5,6 +5,9 @@
  */
 package edu.vanier.physnics.UniformCircularMotionSimulation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 /**
  *
  * @author Admin
@@ -20,12 +23,12 @@ public class FormulasTest {
     @org.junit.jupiter.api.Test
     public void testCalculateAccelerationCentripetal() {
         System.out.println("calculateAccelerationCentripetal");
-        Car car = null;
-        double expResult = 0.0;
+        Car car = new Car(25, 10, 10);
+        double expResult = 62.5;
         double result = Formulas.calculateAccelerationCentripetal(car);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, result, 0.1);
+        System.out.println(expResult);
+        System.out.println(result);
     }
 
     /**
@@ -34,12 +37,14 @@ public class FormulasTest {
     @org.junit.jupiter.api.Test
     public void testCalculateForce() {
         System.out.println("calculateForce");
-        Car car = null;
-        double expResult = 0.0;
+        Car car = new Car(25, 10, 10);
+        double expResult = 625;
+        car.setAccelerationcentr(Formulas.calculateAccelerationCentripetal(car));
+        System.out.println(car.getAccelerationcentr());
         double result = Formulas.calculateForce(car);
-        assertEquals(expResult, result, 0.0);
+        System.out.println(expResult);
+        System.out.println(result);
+        assertEquals(expResult, result, 0.1);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+    }    
 }
