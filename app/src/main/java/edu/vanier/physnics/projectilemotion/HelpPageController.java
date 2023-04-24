b84c1ead29c919ff273ed499d8301985b9eccc51
adarax <adam88183@gmail.com>
2023-04-24 10:37:57 -0400

    Fix compile time error involving escape characters

--- app/src/main/java/edu/vanier/physnics/projectilemotion/HelpPageController.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/HelpPageController.java
@@ -27,18 +27,18 @@ public class HelpPageController {
     
     @FXML
     public void initialize() {
-        mediaButtonText.setText("The green “Play” button will start the animation.\n"
-                + "The yellow “Pause” button will stop the animation.\n"
-                + "The red “Restart” button will return the ball to its original position.");
+        mediaButtonText.setText("The green \"Play\" button will start the animation.\n"
+                + "The yellow \"Pause\" button will stop the animation.\n"
+                + "The red \"Restart\" button will return the ball to its original position.");
         
         sliderText.setText("Click & Drag the sliders to change the values. \n"
                 + "     Initial velocity will change the velocity of the ball the moment it is shot from the cannon. \n"
                 + "     Launch angle changes the angle at which the ball is shot. \n"
                 + "     Gravitational acceleration is the downwards force applied to the ball. ");
         
-        graphButtonText.setText("The “Graph” button will open a new page with graphs that represent the simulation. ");
+        graphButtonText.setText("The \"Graph\" button will open a new page with graphs that represent the simulation. ");
         
-        clearValuesText.setText("The “Clear values” button will reset the sliders to their default position. ");
+        clearValuesText.setText("The \"Clear values\" button will reset the sliders to their default position. ");
     }
    
 }
