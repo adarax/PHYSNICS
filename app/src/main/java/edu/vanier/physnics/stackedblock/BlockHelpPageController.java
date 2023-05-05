f4a352ab4cd85f51311102717a2a4c269a019391
Adam Purcell <adam88183@gmail.com>
2023-05-04 23:45:01 -0400

    Refactor help page using gridpane and textflow elements

--- app/src/main/java/edu/vanier/physnics/stackedblock/BlockHelpPageController.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/BlockHelpPageController.java
@@ -6,6 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /**
@@ -14,11 +22,28 @@ import javafx.stage.Stage;
  */
 public class BlockHelpPageController {
 
-    private final Stage helpWindow;
+    private Stage helpWindow;
+
+    private final String HELP_PARAGRAPH = """
+                                          Drag the force sliders to adjust the forces applied on M1 and M2.\n
+                                          Drag the angle sliders to adjust the angle (to the horizontal) that the forces will have.\n
+                                          Drag the friction sliders to adjust the friction coefficient of that surface.\n
+                                          Drag the mass sliders to adjust the masses of M1 and M2.\n
+                                          Check or uncheck the show/hide vectors button to show or hide the free body diagram of the forces in the block system.\n
+                                          Press the clear values button to reset all sliders to their default values.\n
+                                          The play button plays the animation of the simulation.\n
+                                          The pause button pauses the animation of the simulation. The simulation can be started again using the play button.\n
+                                          The stop button stops the simulation and resets it to the beginning.\n
+                                          The settings tab has the option to change the colors of various objects in the simulation, such as the background and the color of the blocks / force vectors.\n
+                                          The file tab has buttons to quit the program or to return to the main menu.\n
+                                          The \u201cmoon shaped\u201d button sets the application theme to dark mode if on light mode and vice versa.""";
 
     @FXML
     private MFXButton buttonReturnToSimulation;
 
+    @FXML
+    private TextFlow textFlowTextArea;
+    
     public BlockHelpPageController(Stage stage)
     {
         this.helpWindow = stage;
@@ -27,6 +52,17 @@ public class BlockHelpPageController {
     @FXML
     public void initialize()
     {
+        Text helpText = new Text(HELP_PARAGRAPH);
+        helpText.setStyle("-fx-font-size: 21; -fx-text-alignment: justify; -fx-line-spacing: 2;");        
+        textFlowTextArea.getChildren().add(helpText);
+        
+        BorderStroke borderStroke = new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+                new CornerRadii(5), new BorderWidths(1));
+        Border border = new Border(borderStroke);
+        
+        textFlowTextArea.setStyle("-fx-padding: 10px;");
+        textFlowTextArea.setBorder(border);
+        
         buttonReturnToSimulation.setOnAction(click -> handleReturnToSimulation());
     }
 
