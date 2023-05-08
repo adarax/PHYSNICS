e7d0e27a2af0fcd8b36da2bef1bd42348182fe94
vireshpatel43 <viresh.p2003@gmail.com>
2023-05-07 22:04:04 -0400

    Animates ball based on slider values, moves ball to different positions based on cannon position

--- app/src/main/java/edu/vanier/physnics/projectilemotion/Animation.java
+++ app/src/main/java/edu/vanier/physnics/projectilemotion/Animation.java
@@ -4,6 +4,8 @@
  */
 package edu.vanier.physnics.projectilemotion;
 
+import edu.vanier.physnics.conservation.Ball;
+import edu.vanier.physnics.conservation.Settings;
 import java.awt.Point;
 import javafx.animation.PathTransition;
 import javafx.animation.RotateTransition;
@@ -11,11 +13,14 @@ import javafx.animation.Timeline;
 import javafx.geometry.Point3D;
 import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.QuadCurveTo;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
@@ -28,50 +33,42 @@ public class Animation {
     
     PathTransition pathTransition = new PathTransition();
     
-    public void playAnimation(Circle ball, double launchAngle, double gravityAccelMPSS, double initialVelocityMPS) {
+    double maxHeightM;
+    double xDisplacementM;
+    double flightTimeS;
+    
+    public void playAnimation(Circle ball, ImageView cannon, QuadCurve quadCurve, double launchAngleDeg, double gravityAccelMPSS, double initialVelocityMPS) {
         
         Path path = new Path();
         
-        double maxHeightM = Equations.getMaxHeight(launchAngle, initialVelocityMPS, gravityAccelMPSS);
-        double xDisplacementM = Equations.getXdisplacement(launchAngle, initialVelocityMPS, gravityAccelMPSS);
-        double flightTimeS = Equations.getFlightTime(launchAngle, initialVelocityMPS, gravityAccelMPSS);
+        maxHeightM = Equations.getMaxHeight(launchAngleDeg, initialVelocityMPS, gravityAccelMPSS);
+        xDisplacementM = Equations.getXdisplacement(launchAngleDeg, initialVelocityMPS, gravityAccelMPSS);
+        flightTimeS = Equations.getFlightTime(launchAngleDeg, initialVelocityMPS, gravityAccelMPSS);
         
         // Set the scaling from meters to pixels 
         // TODO: Scale the y-axis 
         double xDisplacementPX = xDisplacementM * 50;
-        double maxHeightPX =  - maxHeightM;
         
-
+        double cannonEdgeX = 135 + ((Math.cos(Math.toRadians(launchAngleDeg))) * 67.27);
+        double cannonEdgeY = 790 - ((Math.sin(Math.toRadians(launchAngleDeg))) * 67.27);
+        
         MoveTo moveTo = new MoveTo();
         // Sets initial position of ball
-        moveTo.setX(maxHeightM);
-        moveTo.setY(800);
-
-        // Projectile motion can be reresented by a quadratic curve
+        moveTo.setX(cannonEdgeX);
+        moveTo.setY(cannonEdgeY);
+        
         QuadCurveTo quadTo = new QuadCurveTo();
         
+        quadTo.setControlX((cannonEdgeX + xDisplacementPX) / 2);
+        //You just got the value of height. Now make sure it works with the y-axis!!!!
+        quadTo.setControlY(835 - (scaleHeightToPixels(launchAngleDeg, xDisplacementPX)));
         
-        
-        //TODO: Add exception handling so that the animation doesn't exceed the pixel boundaries
-        
-        /**
-         * Max right is 1560px
-         * Max left is 0px
-         * Ceiling is -760px
-         * Floor is at 29px
-         * 
-         * Use a scale of 10 (1560px = 156 meters)
-         * So grab the values and X10
-         */
-        
-        
-        // Sets a point on the arc (in this case the max height or middle of the arc)
-        quadTo.setControlX((xDisplacementPX - 50) / 2);
-        quadTo.setControlY(maxHeightPX);
         
         // Final point (final displacement)
-        quadTo.setX(xDisplacementPX);
-        quadTo.setY(800);
+        quadTo.setX(70 + xDisplacementPX);
+        quadTo.setY(835);
+        
+        
         path.getElements().add(moveTo);
         path.getElements().add(quadTo);
 
@@ -79,19 +76,43 @@ public class Animation {
         pathTransition.setPath(path);
         pathTransition.setNode(ball);
         pathTransition.play();
-
+       
     }
 
     public void pauseAnimation() {
         pathTransition.pause();
     }
     
+    public void resetBall(Circle projectileBall) {
+        projectileBall.getParent().getChildrenUnmodifiable().remove(projectileBall);
+        projectileBall = new Circle(10);
+        projectileBall.setCenterX(135);
+        projectileBall.setCenterY(790);
+        ((Pane) projectileBall.getParent()).getChildren().add(projectileBall);
+
+    }
+    
+
     public void rotateCannon(ImageView cannonBarrel, double launchAngle) {
         RotateTransition rotate = new RotateTransition();
         rotate.setToAngle(-launchAngle);
         rotate.setNode(cannonBarrel);
         rotate.play();
     }
+    
+    
+    /**
+     * Method that takes parameters for calculations in meters and converts them 
+     * to pixels to be used in the animation.
+     * @param launchAngleDeg
+     * @param xDisplacementMeters 
+     */
+    public double scaleHeightToPixels(double launchAngleDeg, double xDisplacementPixels) {
+        double controlY = Math.tan(Math.toRadians(launchAngleDeg)) * ((70 + xDisplacementPixels) / 2);
+        return controlY;
+    }
+    
+    
     
     
 }
---
+++ app/src/test/java/edu/vanier/physnics/projectilemotion/AnimationTest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.vanier.physnics.projectilemotion;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author vires
+ */
+public class AnimationTest {
+    /**
+     * Test of scaleHeightToPixels method, of class Animation.
+     */
+    @Test
+    public void testScaleHeightToPixels() {
+        System.out.println("scaleHeightToPixels");
+        double launchAngleDeg = 30;
+        double xDisplacementPixels = 100;
+        Animation instance = new Animation();
+        double expResult = 49.07;
+        double result = instance.scaleHeightToPixels(launchAngleDeg, xDisplacementPixels);
+        assertEquals(expResult, result, 0.1);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+    }
+    
+}
