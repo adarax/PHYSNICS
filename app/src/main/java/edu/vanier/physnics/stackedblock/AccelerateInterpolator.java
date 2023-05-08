95224f012a5e40c228d31857a593fea858432fb5
Adam Purcell <2125602@edu.vaniercollege.qc.ca>
2023-05-08 09:53:58 -0400

    Add Javadoc

--- app/src/main/java/edu/vanier/physnics/stackedblock/AccelerateInterpolator.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/AccelerateInterpolator.java
@@ -16,9 +16,18 @@ public class AccelerateInterpolator extends Interpolator {
     }
     
     @Override
+    /**
+     * Calculates what the x position of the animated object should be as time
+     * progresses using the kinematic motion equation x = 1/2 * a * t^2.
+     * 
+     * x is the distance from the initial position of the animated object
+     * a is the acceleration
+     * t is the time elapsed
+     * 
+     * @param time the time elapsed
+     */
     protected double curve(double time)
     {
-        // 0.5 * a * t^2 = x when initial velocity is 0
-        return 0.5 * factor * Math.pow(time, 2);
+        return 0.5 * Math.abs(factor) * Math.pow(time, 2);
     }
 }
