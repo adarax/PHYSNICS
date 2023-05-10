1228a1ca3f7a585f65af0b8c0753b522455055a2
adarax <adam88183@gmail.com>
2023-05-10 15:00:37 -0400

    Implement Settings to preserve configurations of simulation

--- app/src/main/java/edu/vanier/physnics/stackedblock/Settings.java
+++ app/src/main/java/edu/vanier/physnics/stackedblock/Settings.java
@@ -5,5 +5,15 @@ package edu.vanier.physnics.stackedblock;
  * @author adam8
  */
 public class Settings {
-    
+    public static boolean isDark = true;
+
+    public static boolean isDark()
+    {
+        return isDark;
+    }
+
+    public static void setIsDark(boolean isDark)
+    {
+        Settings.isDark = isDark;
+    }
 }
