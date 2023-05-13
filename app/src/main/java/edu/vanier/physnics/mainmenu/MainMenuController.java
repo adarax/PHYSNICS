012a6a5c23878f40720a75efc04601d00f0ba8ba
Adam Purcell <adam88183@gmail.com>
2023-05-12 20:17:26 -0400

    Add exit button to main menu

--- app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
+++ app/src/main/java/edu/vanier/physnics/mainmenu/MainMenuController.java
@@ -6,16 +6,17 @@ import edu.vanier.physnics.conservation.ConservationController;
 import edu.vanier.physnics.projectilemotion.ProjectileController;
 import edu.vanier.physnics.stackedblock.BlockFrontEndController;
 import java.io.IOException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
+ * Controller class for the main menu of the program.
  *
  * @author adarax
  */
@@ -38,34 +39,54 @@ public class MainMenuController extends App {
     @FXML
     private ImageView buttonCentripetal;
 
+    @FXML
+    private ImageView buttonExit;
+
+    private final Image IMAGE_EXIT_BUTTON = new Image(getClass().getResourceAsStream("/images/exit_icon.png"));
+
+    /**
+     * Default constructor for the MainMenuController. Takes a Stage object as a
+     * parameter in order to place the main menu Scene onto it.
+     *
+     * @param stage the Stage holding the Scene
+     */
     public MainMenuController(Stage stage)
     {
         this.stage = stage;
     }
 
-    public void openNewScene(String type)
+    private void openNewScene(String type)
     {
         FXMLLoader loader = null;
-        if (type.equals("stackedblock"))
+        switch (type)
         {
-            loader = new FXMLLoader(getClass().getResource("/fxml/stackedblock.fxml"));
-            BlockFrontEndController controller = new BlockFrontEndController();
-            loader.setController(controller);
-        } else if (type.equals("conservation"))
-        {
-            loader = new FXMLLoader(getClass().getResource("/fxml/conservation.fxml"));
-            ConservationController controller = new ConservationController();
-            loader.setController(controller);
-        } else if (type.equals("projectile"))
-        {
-            loader = new FXMLLoader(getClass().getResource("/fxml/projectile.fxml"));
-            ProjectileController controller = new ProjectileController();
-            loader.setController(controller);
-        } else if (type.equals("UCM"))
-        {
-            loader = new FXMLLoader(getClass().getResource("/fxml/ucm-scene-graph.fxml"));
-            UCMController controller = new UCMController();
-            loader.setController(controller);
+            case "stackedblock" ->
+            {
+                loader = new FXMLLoader(getClass().getResource("/fxml/stackedblock.fxml"));
+                BlockFrontEndController controller = new BlockFrontEndController();
+                loader.setController(controller);
+            }
+            case "conservation" ->
+            {
+                loader = new FXMLLoader(getClass().getResource("/fxml/conservation.fxml"));
+                ConservationController controller = new ConservationController();
+                loader.setController(controller);
+            }
+            case "projectile" ->
+            {
+                loader = new FXMLLoader(getClass().getResource("/fxml/projectile.fxml"));
+                ProjectileController controller = new ProjectileController();
+                loader.setController(controller);
+            }
+            case "UCM" ->
+            {
+                loader = new FXMLLoader(getClass().getResource("/fxml/ucm-scene-graph.fxml"));
+                UCMController controller = new UCMController();
+                loader.setController(controller);
+            }
+            default ->
+            {
+            }
         }
 
         try
@@ -86,35 +107,43 @@ public class MainMenuController extends App {
         stage.show();
     }
 
-    public void handleHelpPressed(MouseEvent e)
+    private void handleHelpPressed()
     {
         System.out.println("Help was requested");
     }
 
+    /**
+     * Initializes the home screen and sets event handlers to the various
+     * buttons.
+     */
     @FXML
     public void initialize()
     {
-        /**
-         * setOnMouseClicked() is used, not setOnAction(), since the buttons are
-         * actually images.
-         */
+        buttonExit.setImage(IMAGE_EXIT_BUTTON);
 
-        buttonConservation.setOnMouseClicked(e ->
+        buttonExit.setOnMouseClicked(press -> handleExit());
+
+        buttonConservation.setOnMouseClicked(press ->
         {
             openNewScene("conservation");
         });
-        buttonStackedBlock.setOnMouseClicked(e ->
+        buttonStackedBlock.setOnMouseClicked(press ->
         {
             openNewScene("stackedblock");
         });
-        buttonProjectile.setOnMouseClicked(e ->
+        buttonProjectile.setOnMouseClicked(press ->
         {
             openNewScene("projectile");
         });
-        buttonCentripetal.setOnMouseClicked(e ->
+        buttonCentripetal.setOnMouseClicked(press ->
         {
             openNewScene("UCM");
         });
-        buttonHelp.setOnMouseClicked(e -> handleHelpPressed(e));
+        buttonHelp.setOnMouseClicked(press -> handleHelpPressed());
+    }
+
+    private void handleExit()
+    {
+        Platform.exit();
     }
 }
